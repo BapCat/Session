47a056c8aaba669b88a561f91530b4cdea870236
Corey Frenette <corey@narwhunderful.com>
2019-01-04 13:30:31 -0400

    Update to PHP 7.1

--- src/DatabaseSessionServiceProvider.php
+++ src/DatabaseSessionServiceProvider.php
@@ -1,40 +1,43 @@
-<?php namespace BapCat\Session;
+<?php declare(strict_types=1); namespace BapCat\Session;
 
-use BapCat\Hashing\Alogorithms\Sha256StrongHash;
-use BapCat\Interfaces\Ioc\Ioc;
+use BapCat\Hashing\Algorithms\Sha256StrongHash;
+use BapCat\Phi\Ioc;
 use BapCat\Remodel\EntityDefinition;
 use BapCat\Remodel\Registry;
 use BapCat\Services\ServiceProvider;
 
 class DatabaseSessionServiceProvider implements ServiceProvider {
+  /** @var Ioc $ioc */
   private $ioc;
+
+  /** @var Registry $remodel */
   private $remodel;
-  
+
   public function __construct(Ioc $ioc, Registry $remodel) {
     $this->ioc     = $ioc;
     $this->remodel = $remodel;
   }
-  
-  public function register() {
+
+  public function register(): void {
     $def = new EntityDefinition(Session::class);
     $def->id(Sha256StrongHash::class)->mapsTo('token');
     $def->required('data', Text::class);
     $def->timestamps();
-    
+
     $this->remodel->register($def);
-    
+
     $this->ioc->singleton(SessionGateway::class, SessionGateway::class);
     $this->ioc->singleton(SessionRepository::class, SessionRepository::class);
-    
+
     $this->ioc->singleton(SessionManager::class, function() {
       $manager = new SessionManager();
       $manager->setHandler($this->ioc->make(DatabaseSessionStorage::class));
-      
+
       return $manager;
     });
   }
-  
-  public function boot() {
+
+  public function boot(): void {
     $this->ioc->make(SessionManager::class)->open();
   }
 }
--- src/TransientSessionServiceProvider.php
+++ src/TransientSessionServiceProvider.php
@@ -1,25 +1,26 @@
-<?php namespace BapCat\Session;
+<?php declare(strict_types=1); namespace BapCat\Session;
 
-use BapCat\Interfaces\Ioc\Ioc;
+use BapCat\Phi\Ioc;
 use BapCat\Services\ServiceProvider;
 
 class TransientSessionServiceProvider implements ServiceProvider {
+  /** @var Ioc $ioc */
   private $ioc;
-  
+
   public function __construct(Ioc $ioc) {
     $this->ioc = $ioc;
   }
-  
-  public function register() {
-    $this->ioc->singleton(SessionManager::class, function() {
+
+  public function register(): void {
+    $this->ioc->singleton(SessionManager::class, function(): SessionManager {
       $manager = new SessionManager();
       $manager->setHandler($this->ioc->make(TransientSessionStorage::class));
-      
+
       return $manager;
     });
   }
-  
-  public function boot() {
+
+  public function boot(): void {
     $this->ioc->make(SessionManager::class)->open();
   }
 }
